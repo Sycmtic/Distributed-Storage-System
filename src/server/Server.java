4d556031795a7d9fcc512cae14aabfcc46ecaf06
melissawang0511 <wang.xinyu2@husky.nue.edu>
2020-04-22 16:29:21 -0700

    additional basic framework

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,4 +1,11 @@
 package server;
 
 public class Server {
+    // service to gain client's account information (what files he owns)
+
+    // service to add a file
+
+    // service to share a file
+
+    // service to change a file content (title) and notify
 }
---
+++ src/utility/Message.java
@@ -0,0 +1,4 @@
+package utility;
+
+public abstract class Message {
+}
